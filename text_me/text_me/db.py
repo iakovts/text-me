5c4c5282959f0fc56cd10176969f59288471531f
iakovts <itsouros@auth.gr>
2024-02-10 19:18:20 +0200

    Update with new endpoint for fetching latest database data

--- text_me/text_me/db.py
+++ text_me/text_me/db.py
@@ -1,11 +1,18 @@
 import asyncio
+import datetime
+from datetime import timedelta
+
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 
-from .settings import config
+try:
+    from .settings import config
+except ImportError:
+    from settings import config
 
 
+datetime_storage = datetime.datetime.now() 
 
 client = AsyncIOMotorClient(
     config.get("host", "localhost"), config.get("port", 27017)
@@ -15,7 +22,7 @@ post_collection = db.collection
 
 
 async def write_post(data: dict[str, str]):
-    document = {"from": data["from"], "text": data["user_text"]}
+    document = {"from": data["from"], "text": data["user_text"], "datetime": datetime.datetime.now()}
     result = await db.post_collection.insert_one(document)
     return result.inserted_id
 
@@ -23,6 +30,18 @@ async def write_post(data: dict[str, str]):
 async def find_by_id(ObjID):
     document = await db.post_collection.find_one(ObjID)
     return document
+
+async def find_all(last_fetch):
+    if not last_fetch:
+        # First run - fetch all
+        last_fetch = datetime.datetime.now() - timedelta(seconds=20000)
+    else:
+        last_fetch = datetime.datetime.now() - last_fetch
+    cursor = db.post_collection.find({
+        "datetime": {
+            "$gte": last_fetch} },
+                                     {"from": 1, "text": 1, "_id": 0}).limit(5)
+    return await cursor.to_list(length=5)
 
 
 async def ping_server():
@@ -43,6 +62,3 @@ async def test_db():
     inserted_id = await write_post({"from": "me", "text": "bla"})
     doc = await find_by_id(inserted_id)
 
-
-if __name__ == "__main__":
-    asyncio.run(test_db())
--- text_me/text_me/views.py
+++ text_me/text_me/views.py
@@ -1,8 +1,11 @@
-from aiohttp import web
-
+import datetime
+import json 
+from typing import Any
+from aiohttp import WSMsgType, web
+import aiohttp
 import aiohttp_jinja2
 
-from .db import write_post
+from .db import find_all, write_post
 
 
 class IndexHandle(web.View):
@@ -10,7 +13,6 @@ class IndexHandle(web.View):
     async def post(self) -> None:
         form = await self.request.post()
         inserted_id = await write_post(form)
-        # import pdb; pdb.set_trace()
         raise web.HTTPFound(self.request.app.router["play_again"].url_for())
 
     @aiohttp_jinja2.template("text_form.html")
@@ -18,8 +20,32 @@ class IndexHandle(web.View):
         return {}
 
 
+class FetchHandler(web.View):
+    last_fetch = None
+    async def get(self):
+        resp = await find_all(FetchHandler.last_fetch)
+        FetchHandler.last_fetch = datetime.datetime.now()
+        return web.json_response(resp)
+
+
+
 @aiohttp_jinja2.template("play_again.html")
 async def play_again(request):
     if request.method == "POST":
         location = request.app.router["index"].url_for()
         raise web.HTTPFound(location)
+
+
+async def websocket_handler(request):
+
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                if msg.data == "close":
+                    await ws.close()
+                else:
+                    await ws.send_str(msg.data) 
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print("ws connection closed with error %s" % ws.exception())
+
