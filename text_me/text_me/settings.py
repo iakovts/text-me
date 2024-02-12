739783e94f67972bf0c20ba530e29f448e2a135a
iakovos <itsouros@auth.gr>
2024-02-12 15:58:11 +0200

    Fix Docker

--- text_me/text_me/settings.py
+++ text_me/text_me/settings.py
@@ -4,7 +4,12 @@ import yaml
 from typing import Any
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
+
 config_path = BASE_DIR / "configs" / "text_me.yaml"
+if not config_path.is_file(): # running in docker
+    # also change BASE_DIR here, kinda hacky but anyway
+    BASE_DIR = pathlib.Path("/app/text_me")
+    config_path = BASE_DIR / "configs/text_me.yaml"
 
 
 def get_config(path: pathlib.Path) -> dict[str, Any]:
